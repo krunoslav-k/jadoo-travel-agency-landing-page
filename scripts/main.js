155aa0e8cf3f2ed43116e185c0ad86ceb41d2a50
Krunoslav Kazalicki <krunoslavk@hotmail.com>
2025-09-14 13:41:24 +0200

    feat: add pagination dots to tesimonials slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
 const cards = document.querySelectorAll(".testimonials__slider__card");
+const paginationDots = document.querySelectorAll(
+  ".testimonials__pagination-dot"
+);
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
 
@@ -8,8 +11,9 @@ function updateCards() {
   cards.forEach((card, index) => {
     card.classList.remove("active", "prev", "next");
 
-    if (index === current) card.classList.add("active");
-    else if (
+    if (index === current) {
+      card.classList.add("active");
+    } else if (
       index === current - 1 ||
       (current === 0 && index === cards.length - 1)
     )
@@ -22,14 +26,24 @@ function updateCards() {
   });
 }
 
+function updateDots() {
+  paginationDots.forEach((dot) => {
+    dot.classList.remove("active");
+  });
+  paginationDots[current].classList.add("active");
+}
+
 prevBtn.addEventListener("click", () => {
   current = (current - 1 + cards.length) % cards.length;
   updateCards();
+  updateDots();
 });
 
 nextBtn.addEventListener("click", () => {
   current = (current + 1) % cards.length;
   updateCards();
+  updateDots();
 });
 
 updateCards();
+updateDots();
